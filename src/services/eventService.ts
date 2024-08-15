00e2b7086250090cd6c70113de21b7aba5895e7d
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-08-15 12:00:25 +0200

    create event services for Event API calls

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,7 +1,38 @@
-export const createEvent = () => {};
+import axios from "axios";
 
-export const getEvents = () => {};
+const API_URL = "http://localhost:5000/api/event";
 
-export const updateEvent = () => {};
+export const getEvents = async () => {
+  const response = await axios.get(API_URL, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return response.data;
+};
 
-export const deleteEvent = () => {};
+export const createEvent = async (event) => {
+  const response = await axios.post(API_URL, event, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return response.data;
+};
+
+export const updateEvent = async (id, event) => {
+  const response = await axios.put(`${API_URL}/${id}`, event, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return response.data;
+};
+
+export const deleteEvent = async (id) => {
+  await axios.delete(`${API_URL}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+};
