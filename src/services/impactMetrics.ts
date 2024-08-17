1f7acf09f76a215fbbd579f677c788f5efffc27f
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-08-17 12:59:06 +0200

    add impactMetrics service

--- src/services/impactMetrics.ts
+++ src/services/impactMetrics.ts
@@ -1,3 +1,19 @@
+import axios from "axios";
 
+const API_URL = "http://localhost:5000/api/impact";
 
-export const getImpactMetrics = () => {};
+export const getImpactMetrics = async () => {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(
+      "Error occurred while getting impact metrics from server",
+      error
+    );
+  }
+};
